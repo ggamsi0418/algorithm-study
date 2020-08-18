ca79ea2ffdd424c305fcb762e02d71f3947a1f7c
jb <ggamsi0418@naver.com>
2020-08-18 23:20:07 +0900

    Section05 - 5th commit

--- section05/problem03.py
+++ section05/problem03.py
@@ -1,15 +1,44 @@
 """
     [프로그래머스] 단어변환
     https://programmers.co.kr/learn/courses/30/lessons/43163
+
+    [참고 풀이]
+    https://copy-driven-dev.tistory.com/123
 """
 from collections import deque
 
 
-def solution(begin, target, words):
-    if target not in words:
-        return 0
+def searchDiff(a, b, n):
+    ret = 0
+    for i in range(n):
+        if a[i] != b[i]:
+            ret += 1
 
+    if ret == 1:
+        return True
+    else:
+        return False
+
+
+def solution(begin, target, words):
     answer = 0
+
+    v = dict()
+    len_words = len(begin)
+    queue = deque()
+    queue.append((begin, 0))
+    v[begin] = True
+
+    while queue:
+        cur, l = queue.popleft()
+
+        for word in words:
+            if searchDiff(word, cur, len_words) and word not in v:
+                queue.append((word, l + 1))
+                v[word] = True
+                if word == target:
+                    answer = l + 1
+
     return answer
 
 
--- section05/problem04.py
+++ section05/problem04.py
@@ -1,13 +1,32 @@
 """
     [프로그래머스] 여행경로
     https://programmers.co.kr/learn/courses/30/lessons/43164
+
+    [참고 풀이]
+    https://jisun-rea.tistory.com/entry/python-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Level3-%EC%97%AC%ED%96%89%EA%B2%BD%EB%A1%9C-DFS
 """
-from collections import deque
 
 
 def solution(tickets):
-    answer = []
-    return answer
+    tickets.sort(reverse=True)
+    routes = dict()
+    for t1, t2 in tickets:
+        if t1 in routes:
+            routes[t1].append(t2)
+        else:
+            routes[t1] = [t2]
+
+    st = ["ICN"]
+    ans = []
+
+    while st:
+        top = st[-1]
+        if top not in routes or len(routes[top]) == 0:
+            ans.append(st.pop())
+        else:
+            st.append(routes[top].pop())
+    ans.reverse()
+    return ans
 
 
 tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
